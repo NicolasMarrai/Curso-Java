8096c652979a8b28e27880bb5a5c6a0e7c7643bc
NicolasMarrai <nicolasmarrai.dev@gmail.com>
2026-05-19 17:10:16 -0300

    finalizando

--- 004. Estrutura Repetitiva/exercicio09/src/program.java
+++ 004. Estrutura Repetitiva/exercicio09/src/program.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 public class program {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		int N = sc.nextInt();
-		
-		for(int i = 1; i <= N; i++) {
-			
+
+		for (int i = 1; i <= N; i++) {
+
 			int result = N % i;
 			if (result == 0) {
 				System.out.println(i);
-			}		
+			}
 		}
 		sc.close();
 	}
