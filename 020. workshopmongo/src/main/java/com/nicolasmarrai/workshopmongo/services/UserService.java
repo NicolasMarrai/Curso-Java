d45639c8a16f2fb1c926c11e3a111db4983f96f0
Nicolas Marrai <nicolasmarrai.dev@gmail.com>
2026-06-26 13:10:04 -0300

    Operação de instanciação da base de dados

--- 020. workshopmongo/src/main/java/com/nicolasmarrai/workshopmongo/services/UserService.java
+++ 020. workshopmongo/src/main/java/com/nicolasmarrai/workshopmongo/services/UserService.java
@@ -6,13 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nicolasmarrai.workshopmongo.domain.User;
-import com.nicolasmarrai.workshopmongo.repository.UserRespository;
+import com.nicolasmarrai.workshopmongo.repository.UserRepository;
 
 @Service
 public class UserService {
 	
 	@Autowired
-	private UserRespository repo;
+	private UserRepository repo;
 	
 	public List<User> findAll(){
 		return repo.findAll();
