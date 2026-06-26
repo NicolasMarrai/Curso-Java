5d424da74fcd2902124307be4bd5ee99feea1853
Nicolas Marrai <nicolasmarrai.dev@gmail.com>
2026-06-26 15:28:14 -0300

    Inserção de usuário com POST

--- 020. workshopmongo/src/main/java/com/nicolasmarrai/workshopmongo/services/UserService.java
+++ 020. workshopmongo/src/main/java/com/nicolasmarrai/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nicolasmarrai.workshopmongo.domain.User;
+import com.nicolasmarrai.workshopmongo.dto.UserDTO;
 import com.nicolasmarrai.workshopmongo.repository.UserRepository;
 import com.nicolasmarrai.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User Insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
